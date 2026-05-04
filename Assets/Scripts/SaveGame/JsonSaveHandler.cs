0921db00894b7d4f2e8d8892c74d0200f9957a4e
DESKTOP-9UN6IRC\User <thanakorn.ekk.03@gmail.com>
2026-05-04 16:12:41 +0700

    update todo

--- Assets/Scripts/SaveGame/JsonSaveHandler.cs
+++ Assets/Scripts/SaveGame/JsonSaveHandler.cs
@@ -6,14 +6,16 @@ namespace SaveGame
     {
         public static void JsonSave()
         {
-            // TODO : implement pull player's data to be serialized
+            // TODO : pull game setting to be serialized
+            // TODO : pull player's data to be serialized (player name, level, health, status, inventory, etc.)
             // TODO : serialize game data and save to persistent data path
         }
 
         public static void JsonLoad()
         {
             // TODO : pull saved data from persistent data path and deserialize game data
-            // TODO : implement write loaded data to player data
+            // TODO : apply loaded game setting
+            // TODO : apply loaded data to player data (player name, level, health, status, inventory, etc.)
         }
     }
 }
