a54e22c4a0a2f70aaf751211ede0e539b0e09fd3
DESKTOP-9UN6IRC\User <thanakorn.ekk.03@gmail.com>
2026-05-15 16:02:55 +0700

    change skilldata to individual class and removed unused class

--- Assets/Scripts/Character/BossData.cs
+++ Assets/Scripts/Character/BossData.cs
@@ -7,18 +7,9 @@ namespace Data
     [JsonObject]
     public class BossData : EnemyData
     {
-        [JsonProperty] private List<BossSkill> skills = new List<BossSkill>();
+        [JsonProperty] private List<SkillData> skills = new List<SkillData>();
         [JsonProperty] private string activeArea = "boss_room";
         [JsonProperty] private int phase = 1;
         [JsonProperty] private float rageThreshold = 0.3f;
-
-        [JsonObject]
-        public class BossSkill
-        {
-            [JsonProperty] private float cooldown;
-            [JsonProperty] private float damageMultiplier;
-            [JsonProperty] private float range;
-            [JsonProperty] private float areaOfEffect;
-        }
     }
 }
